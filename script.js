28c7579b36f6b7a9c530a6dba19f824253ddeea2
Elhosseny2 <moelhosiny8@gmail.com>
2024-11-15 18:01:37 +0200

    Change the LOGIC !
    i decided to change the program's logic a little
    bit, which make it easier (for me) to handle at
    this point and GTD so i got more time for other
    life things. (that is a message for me)

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let addBTN = document.querySelector("#add");
 let multiplyBTN = document.querySelector("#multiply");
 let subtractBTN = document.querySelector("#subtract");
 let divideBTN = document.querySelector("#divide");
-let numberBTN = document.querySelectorAll(".num");
+let one = document.querySelector(".one")
+let two = document.querySelector(".two")
+let three = document.querySelector(".three")
+let four = document.querySelector(".four")
+let five = document.querySelector(".five")
+let six = document.querySelector(".six")
+let seven = document.querySelector(".seven")
+let eight = document.querySelector(".eight")
+let nine = document.querySelector(".nine")
+let dot = document.querySelector(".dot")
 let clearBTn = document.querySelector(".clear");
 let display = document.querySelector("#display");
 function add(a, b) {
@@ -20,26 +29,26 @@ function divide(a, b) {
   }
   return a / b;
 }
-let Number1;
-let operator;
-let Number2;
+let Number1 = 0;
+let operator ;
+let Number2 = 0;
 // let displayInput = display;
 
 //Create a new function (operate) that takes an operator and two numbers and then calls one of the above functions on the numbers.
 function operate(num1, num2, operator) {
   switch (operator) {
     case "+":
-      num1 + num2;
+      add(num1 + num2)
       break;
     case "-":
-      num1 - num2;
+      subtract(num1 - num2)
       break;
     case "*":
-      num1 * num2;
+      multiply(num1 * num2);
       break;
     case "/":
-      num1 / num2;
+      divide(num1 / num2);
       break;
   }
 }
-
+``
